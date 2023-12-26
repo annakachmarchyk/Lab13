54b68659c60ad75b05652495640ae29e2e6456f5
annakachmarchyk <116542188+annakachmarchyk@users.noreply.github.com>
2023-12-26 18:56:26 +0200

    Delete Lab12 directory

--- Lab12/src/main/java/org/example/task2/StampingVisitor.java
+++
@@ -1,9 +0,0 @@
-package org.example.task2;
-
-import java.util.Map;
-
-public interface StampingVisitor {
-    Map<String, Object> onGroupStart(Group<?> group, Map<String, String> headers);
-    void onGroupEnd(Group<?> group, Map<String, String> headers);
-    Map<String, Object> onSignature(Signature<?> signature, Map<String, String> headers);
-}
